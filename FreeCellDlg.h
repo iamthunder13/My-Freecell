3978992174160462cb9d29bf91675d819c7ed785
iamthunder13 <35380090+iamthunder13@users.noreply.github.com>
2021-01-26 16:56:56 -0700

    Add files via upload

---
+++ FreeCellDlg.h
@@ -0,0 +1,34 @@
+
+// FreeCellDlg.h : header file
+//
+
+#pragma once
+
+
+// CFreeCellDlg dialog
+class CFreeCellDlg : public CDialogEx
+{
+// Construction
+public:
+	CFreeCellDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FREECELL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
